25eabd8dc0f51b7379c711c0b0a4273bcef361dd
KFC <kfchang@fstop.com.tw>
2019-05-06 16:40:48 +0800

    Using the Session Middleware

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.user(
+app.use(
   session({ secret: 'my secret', resave: false, saveUninitialized: false })
 );
 
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,21 @@
 exports.getLogin = (req, res, next) => {
 //console.log(req.get('Cookie').split(';')[0].trim().split('=')[1]);
-  const isLoggedIn = req
-    .get('Cookie')
-    .split(';')[0]
-    .trim()
-    .split('=')[1];
+  //   const isLoggedIn = req
+  //     .get('Cookie')
+  //     .split(';')[1]
+  //     .trim()
+  //     .split('=')[1] === 'true';
+  console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true;Max-Age=10');
-    res.redirect('/');
-  };
+  req.session.isLoggedIn = true;
+    //res.setHeader('Set-Cookie', 'loggedIn=true;Max-Age=10');
+  res.redirect('/');
+};
  
